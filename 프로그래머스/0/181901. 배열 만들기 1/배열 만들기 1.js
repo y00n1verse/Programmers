9cd63798685460226070a68c1dfe15eddf38d46d
홍지윤 <h_zhirun@naver.com>
2025-09-05 17:43:17 +0900

    [level 0] Title: 배열 만들기 1, Time: 5.77 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -0,0 +1,10 @@
+function solution(n, k) {
+    let answer = [];
+    
+    for (let i = 1; i <= n; i++) {
+        if (i % k === 0) {
+        answer.push(i)
+     }   
+    }
+    return answer;
+}
